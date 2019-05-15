57b339e2e3cdc5295025b7bbccd6c57a8de40a1e
ABCode <abcode07@gmail.com>
2019-05-15 16:56:47 +0100

    Blog.com
    Responsive Home Page

---
+++ Blog/index.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="icon" type="image/png" href="abb.png">
+	<title> Blog </title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+	<link rel="stylesheet" type="text/css" href="responsive.css">
+</head>
+<body>
+
+<header>
+	
+<h2> Blog <span><small><small> .com </small></small></span> </h2>
+
+<article> Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam. </article>
+
+</header>
+
+<section>
+	
+<div class="d1">
+	
+<img src="a.jpg">
+
+<h3> Art </h3>
+
+<button> START NOW </button>
+
+</div>
+
+<div class="d2">
+	
+<img src="b.jpg">
+
+<h3> Music </h3>
+
+<button> START NOW </button>
+
+</div>
+
+<div class="d3">
+	
+<img src="c.jpg">
+
+<h3> Sport </h3>
+
+<button> START NOW </button>
+
+</div>
+
+</section>
+
+</body>
+</html>
